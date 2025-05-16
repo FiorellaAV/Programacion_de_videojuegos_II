e2d2243e839ffc868c07d65d325cf7cc4ce82fad
AleHutmacher <hutmacheralejo@gmail.com>
2025-05-16 02:09:43 -0300

    add: added buttons and SFX
    - added credits
    - new UI with SFX
    - new PLAY button
    - new CREDITS button
    - new EXIT button

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public Animator introAnimator;
     public Animator menuAnimator;
+    public Animator brickUpAnimator;
 
     public LightController r_eye;
+
+    public GameObject brickUp, brickMid, brickDown;
+    public GameObject buttonPlay, buttonCredits, buttonExit;
+
+
+    [SerializeField] bool clicked = false;
     // Start is called before the first frame update
     void Start()
     {
         HideCursor();
         introAnimator.enabled = true;
-        StartCoroutine(WaitIntroFinish(7));
+        StartCoroutine(WaitIntroFinish(8));
     }
 
     void AnimateMenu()
     {
-        menuAnimator.enabled = true;
+        menuAnimator.SetTrigger("MenuIntro");
     }
 
     IEnumerator WaitIntroFinish(float seconds)
@@ -38,5 +47,43 @@ public class MenuManager : MonoBehaviour
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void ClickBrickUp()
+    {
+        brickUp.GetComponent<AudioSource>().Play();
+        if (!clicked)
+        {
+            clicked = true;
+            brickUpAnimator.SetTrigger("Click");
+            r_eye.TurnOnLight();
+            StartCoroutine(Play());
+        }
+        else
+        {
+            clicked = false;
+            brickUpAnimator.SetTrigger("ClickOff");
+            r_eye.TurnOffLight();
+        }
+        
+        
+    }
+
+    public void Credits()
+    {
+        brickMid.GetComponent<AudioSource>().Play();
+    }
+
+    IEnumerator Play()
+    {
+        buttonExit.GetComponent<Button>().interactable = false;
+        buttonCredits.GetComponent<Button>().interactable = false;
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("MainRoom");
+    }
+
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
     }
 }
